d813de29c2ab8b7fd95db7f151a1ce8c02d8f439
Saurav Punjabi <sauravpunjabi123@gmail.com>
2025-08-19 10:02:29 +0530

    added homepage

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,5 +1,7 @@
+import { resumes } from "~/constants";
 import type { Route } from "./+types/home";
 import Navbar from "~/components/Navbar";
+import ResumeCard from "~/components/ResumeCard";
 export function meta({}: Route.MetaArgs) {
   return [
     { title: "NailTheCV" },
@@ -19,6 +21,17 @@ export default function Home() {
         </div>
     </section>
 
-    {}
-  </main>
+
+    {resumes.length > 0 &&(
+       <div className="resumes-section">
+         {resumes.map((resume) => (
+      <ResumeCard key={resume.id} resume = {resume}/>
+    ))}
+    </div>
+    )}
+   
+
+   
+
+    </main>
 }
